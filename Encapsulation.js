fee87bf80fea0be02627e003251ac8ab54d50f90
Sofiya Polulyakh <sofiyapolulyakh@Sofiyas-MacBook-Pro.local>
2019-01-20 17:17:04 +0200

    Added changeBalance method

--- Encapsulation.js
+++ Encapsulation.js
@@ -8,9 +8,24 @@ function BankAccount(cardNumber, balance) {
         } else {
             return _balance;
         }
+      };
+
+  this.changeBalance = function (pinCode, sum){
+    if (pinCode !== "1234") {
+            console.log("Wrong pin code.");
+        } else {
+        if (_balance > sum)
+        { 
+          _balance -= sum;
+        }
+          return _balance;
+        }
     };
-}
-var account = new BankAccount("1234432156433456", "100500");
+  }
+
+var account = new BankAccount("1234432156433456", 1000);
 console.log(account.cardNumber);
 console.log(account._balance);
 console.log(account.showBalance("1234"));
+console.log(account.changeBalance("1234", 500));
+console.log(account.changeBalance("1234", 5000));
